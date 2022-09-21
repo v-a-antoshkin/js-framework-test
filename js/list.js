5c2447208c3cbc05f9d6e4dfe98519db8f1b6d0a
polyathlon <polyatlon.rsu.edu.ru@gmail.com>
2022-09-21 14:22:00 +0300

    Add Polymer

--- js/list.js
+++ js/list.js
@@ -5,4 +5,5 @@ var List = [
 	{ type: 'naive aurelia', id: 'aurelia', url: './frameworks/aurelia', label: 'Aurelia' },
 	{ type: 'naive backbone', id: 'backbone', url: './frameworks/backbone', label: 'Backbone' },
 	{ type: 'naive angular', id: 'angular', url: './frameworks/angular', label: 'Angular' },
+	{ type: 'naive polymer ', id: 'polymer', url: './frameworks/polymer', label: 'Polymer 1.8' },
 ];
